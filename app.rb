d77152cc37a2a548b1d6881eb0583b211ecbd49d
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-12 21:54:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,7 +15,8 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @num = params[:number].to_i
     @phrase = params[:phrase]
-    num.times do
+    num = @num.to_i
+    @num.times do
       "a"
     end
     #statement = Array.new(@num, @phrase).join("\n")
